cd7c696b367af027cf3173b74bc415d33a47af87
Ryuzo Yamate <iiiiiii.satenanbon@gmail.com>
2022-02-07 15:26:56 +0900

    initial commit

---
+++ FirstXcode/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirstXcode
+//
+//  Created by r_yamate on 2022/02/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
